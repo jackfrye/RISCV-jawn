7daf66090e7efb4aa537cbbb66f42abaf2f07a73
Kelvyloo <kjlu11823@gmail.com>
2019-02-12 19:48:44 -0500

    Init commit ALU hardware class

---
+++ src/single_cycle/ALU.h
@@ -0,0 +1,19 @@
+#ifndef __ALU_H__
+#define __ALU_H__
+
+#include <cstdint>
+
+using namespace std;
+
+class Algo_Logic_Unit
+{
+private:
+    uint32_t alu_result;
+
+public:
+    Algo_Logic_Unit(uint32_t op_1, uint32_t op_2, uint8_t alu_control, bool alu_op);
+    
+    uint32_t get_alu_result() { return alu_result; }
+};
+
+#endif
