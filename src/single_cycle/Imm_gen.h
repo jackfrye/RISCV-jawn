764d6353b5bcd5f8a2206129c899bad4f6a8c3e2
Kelvyloo <kjlu11823@gmail.com>
2019-02-16 17:46:31 -0500

    Changed imm_gen_result type to signed

--- src/single_cycle/Imm_gen.h
+++ src/single_cycle/Imm_gen.h
@@ -9,12 +9,12 @@ using namespace std;
 class Imm_gen 
 {
 private:
-    uint64_t imm_gen_out;
+    int64_t imm_gen_out;
 
 public:
     Imm_gen();
     void set_imm_gen(uint8_t op_code, uint32_t instruction);
-    uint64_t get_imm_gen_result() { return imm_gen_out; }
+    int64_t get_imm_gen_result() { return imm_gen_out; }
 };
 
 #endif
