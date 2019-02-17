dff95b9045c259c787eca73a284f2b3d43be15ed
Kelvyloo <kjlu11823@gmail.com>
2019-02-16 22:30:50 -0500

    Added JAL and JALR cases plus sign extension

--- src/single_cycle/Imm_gen.cpp
+++ src/single_cycle/Imm_gen.cpp
@@ -11,10 +11,36 @@ void Imm_gen::set_imm_gen(uint8_t op_code, uint32_t instruction)
     switch (op_code) {
         case LOAD:     
             tmp = (instruction >> 20) & 0xFFF; // Instruction[31:20]
+
+            if (tmp & 800) {
+                tmp |= ~0xFFF;
+            }
             break;
         case STORE:    
             tmp  =  (instruction >> 7)  & 0x1F;       // Instruction[11:7]
             tmp |= ((instruction >> 25) & 0x3F) << 5; // Instruction[31:25]
+
+            if (tmp & 800) {
+                tmp |= ~0xFFF;
+            }
+            break;
+        case JAL:
+            tmp  = ((instruction << 31) & 0x1)   << 20;  // Instruction[31]
+            tmp |= ((instruction << 12) & 0x3FF) << 10;  // Instruction[21:12]
+            tmp |= ((instruction << 22) & 0x1)   <<  9;  // Instruction[22]
+            tmp |= ((instruction << 23) & 0x7F);         // Instruction[30:23]
+
+            if (tmp & 0x80000) {
+                tmp |= ~0xFFFFF;
+            }
+            tmp = tmp << 1;
+            break;
+        case JALR:
+            tmp = (instruction << 20) & 0xFFF; // Instruction[31:20]
+
+            if (tmp & 800) {
+                tmp |= ~0xFFF;
+            }
             break;
         case BRANCH:
             tmp  = ((instruction >> 31) & 0x1)  << 11; // Instruction[31]
@@ -38,6 +64,10 @@ void Imm_gen::set_imm_gen(uint8_t op_code, uint32_t instruction)
                 tmp = (instruction >> 20) & 0x1F;
 
             tmp = (instruction >> 20) & 0xFFF; // Instruction[31:20]
+
+            if (tmp & 800) {
+                tmp |= ~0xFFF;
+            }
             break;
         default:
             tmp = 0;
