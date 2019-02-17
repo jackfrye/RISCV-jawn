67139587c04089948b0f3814cc9213a509e60ebc
Kelvyloo <kjlu11823@gmail.com>
2019-02-17 13:52:34 -0500

    Added optional cpu_trace argument for script

--- run_riscv_ss.sh
+++ run_riscv_ss.sh
@@ -2,4 +2,9 @@
 
 bash compile.bash single-cycle
 
-./RVSim configs/RISC_V.cfg out/out cpu_traces/cpu_traces_core_0
+if [ -z $1 ]; then
+    echo "No input file included - running default cpu trace"
+    ./RVSim configs/RISC_V.cfg out/out cpu_traces/cpu_traces_core_0
+else
+    ./RVSim configs/RISC_V.cfg out/out $1
+fi
