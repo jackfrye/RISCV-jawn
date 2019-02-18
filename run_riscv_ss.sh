b5714bfaf856c00a75023519f02d621c7d39a62b
jackfrye <jack.frye@comcast.net>
2019-02-17 19:54:48 -0500

    Assembly files for dual and quad core

--- run_riscv_ss.sh
+++ run_riscv_ss.sh
@@ -6,5 +6,16 @@ if [ -z $1 ]; then
     echo "No input file included - running default cpu trace"
     ./RVSim configs/RISC_V.cfg out/out cpu_traces/cpu_traces_core_0
 else
-    ./RVSim configs/RISC_V.cfg out/out $1
+    if [ -z $2 ]; then
+        echo "one arg"
+	./RVSim configs/RISC_V.cfg out/out $1
+    else
+        if [ -z $3 ]; then
+            echo "two arg"
+	    ./RVSim configs/RISC_V_2.cfg out/out $1 $2
+        else
+            echo "four arg"
+            ./RVSim configs/RISC_V_4.cfg out/out $1 $2 $3 $4
+        fi
+    fi
 fi
